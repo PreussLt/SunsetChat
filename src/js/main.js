da73d7f9c8cd1484578cd8bce99ac5e24267a458
Kyle Henselmann <44337037+PreussLt@users.noreply.github.com>
2022-10-27 10:58:14 +0200

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,21 @@
 
+
 function setReload(){
     document.getElementById("checkbox").setAttribute("onclick","reloadCSS(getCookie('user'))");
 
 }
 function  reloadCSS(user) {
-    var text =document.getElementById('text_input').value;
-    sendeDaten(text,user);
-    alert("Nachricht wurde gesendet: "+text);
-    document.location.reload();
+    try {
+        var text =document.getElementById('text_input').value;
+        sendeDaten(text,user);
+        alert("Nachricht wurde gesendet: "+text);
+        document.cookie = "restBtn=true; path=/";
+    }catch (err){
+        alert("Fehler im reloadCss: "+err);
+    }
+
 }
+
 
 async function empfangeDaten(roomId, userName) {
     const user = userName;
@@ -28,6 +35,7 @@ async function empfangeDaten(roomId, userName) {
              myArray[i][y] = arr[1];
         }
 
+        /*
         if (myArray[i][2]==user){
             document.getElementById("room1_box").innerHTML += "<div class=\"chat-om\">"+myArray[i][3]+"</div>";
         }else {
@@ -39,7 +47,26 @@ async function empfangeDaten(roomId, userName) {
 
             document.getElementById("room1_box").innerHTML += "<div class=\"chat-fm\">"+myArray[i][3]+"</div>";
         }
+
+        */
     }
+
+   const rArray = myArray.reverse();
+    document.cookie = "lm="+rArray[0][0]+"; path=/";
+    for (let y =0; y< rArray.length;y++){
+        if (myArray[y][2]==user){
+            document.getElementById("room1_box").innerHTML += "<div class=\"chat-om\">"+myArray[y][3]+"</div>";
+        }else {
+            if (y==0){
+                document.getElementById("room1_box").innerHTML += "<h4>" + myArray[y][2] + ":</h4>";
+            }else if(myArray[y-1][2]!=myArray[y][2]){
+                document.getElementById("room1_box").innerHTML += "<h4>" + myArray[y][2] + ":</h4>";
+            }
+
+            document.getElementById("room1_box").innerHTML += "<div class=\"chat-fm\">"+myArray[y][3]+"</div>";
+        }
+    }
+
 
 }
 async function sendeDaten(data,user){
@@ -57,10 +84,10 @@ async function sendeDaten(data,user){
             }
         })
         .then(function(res){ return res.json(); })
-        .then(function(data){ alert( JSON.stringify( data ) ) })
+        .then(function(data){ console.log( JSON.stringify( data ) ) })
 
     //let myObject =  await fetch("https://chat.web2021.dhbw.scytec.de/room/2/messages");
-    let myText =  await myObject.text();
+    //let myText =  await myObject.text();
 
 }
 
@@ -73,15 +100,24 @@ function login(a){
     }
 }
 
-function scroll_to_end(frame) {
-    var frame = window.frames.DP_Log_frame;
-    var doc   = frame.document;
-    var node  = doc.getElementsByTagName('body')[0].lastChild;
-
-    var y = parseInt(node.offsetTop, 10);
-
-    frame.scrollTo(0, y);
-    alert("f")
+function splitArray(myArray){
+    try {
+        for (let i=0; i<myArray.length;i++){
+            myArray[i] = myArray[i].replaceAll("{","");
+            myArray[i] = myArray[i].replaceAll("[","");
+            myArray[i] = myArray[i].replaceAll("]","");
+            myArray[i] = myArray[i].replaceAll("\"","");
+            myArray[i] = myArray[i].split(",");
+            for (let y =0;y<myArray[i].length;y++){
+                let arr = myArray[i][y].split(":");
+                myArray[i][y] = arr[1];
+            }// End For y
+        }// End For i
+        const rArray = myArray.reverse();
+        return rArray;
+    }catch (err){
+        console.log("Fehler "+err);
+        return null;
+    }
 }
-
 
